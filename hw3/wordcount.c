d781a62ee0eb9df8be4665ee59cf0bb6b1c6d34b
peichi1 <peichi1@calgary.cs.washington.edu>
2024-01-31 03:54:09 -0800

    Initialize skeleton code (i.e. func dec/def)

--- hw3/wordcount.c
+++ hw3/wordcount.c
@@ -1,3 +1,6 @@
+// Name: Perry Chien
+// Date: 1/28/24
+// CSE 374 Homework 3: Wordcount
 /*
     TODO: Include basic information.
 */
@@ -8,8 +11,19 @@
 #define MAXLINE 500
 
 // TODO: Function declarations/prototypes (with appropriate comments).
+// Handles error of no input files passed as argument
+void fileError();
+
+// Processes a single input file and calculates the number of lines, words, and
+// characters based on the specified option (i.e. -l, -w, and -c respectively).
+// If no option is specified, the total number of lines are printed.
+void fileProcessor();
 
 // TODO: Function definitions.
 int main(int argc, char* argv[]) {}
+
+void fileError() {
+
+}
 
 // TODO: Remove all TODO comments (like this one) once you are done!
