fb54aca42495b9574f5088d13a41f9d81d28ca02
peichi1 <peichi1@calgary.cs.washington.edu>
2024-02-25 21:13:16 -0800

    Finished implementation of entire Vector.cc with basic commenting

--- hw7/Vector.cc
+++ hw7/Vector.cc
@@ -6,4 +6,48 @@ Vector::Vector() {
     init(0, 0, 0);
 }
 
-} // namespace vector374
+Vector::Vector(const float x, const float y, const float z) {
+    init(x, y, z);
+}
+
+Vector::Vector(const Vector &v) {
+    init(v.v_[0], v.v_[1], v.v_[2]);
+}
+
+Vector::~Vector() {
+    delete[] v_;
+}
+
+Vector& Vector::operator=(const Vector &rhs) {
+    if (this != &rhs) {
+        delete[] v_;
+        init(rhs.v_[0], rhs.v_[1], rhs.v_[2]);
+    }
+    return *this;
+}
+
+double operator*(const Vector &a, const Vector &b) {
+    return a.v_[0] * b.v_[0] + a.v_[1] * b.v_[1] + a.v_[2] * b.v_[2];
+}
+
+Vector operator*(const double k, const Vector &v) {
+    return Vector(k * v.v_[0], k * v.v_[1], k * v.v_[2]);
+}
+
+Vector operator*(const Vector &v, const double k) {
+    return k * v;
+}
+
+ostream& operator<<(ostream &out, const Vector &v) {
+    out << "(" << v.v_[0] << "," << v.v_[1] << "," << v.v_[2] << ")";
+    return out;
+}
+
+void Vector::init(const float x, const float y, const float z) {
+    v_ = new float[3];
+    v_[0] = x;
+    v_[1] = y;
+    v_[2] = z;
+}
+
+}   // namespace vector374
